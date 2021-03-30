79602b3e9a87dde40a02195fadeb01abae989225
JiangXia-1024 <j17621607346@163.com>
2021-03-30 21:35:03 +0800

    bean作用域

--- src/main/java/com/springboot/springbootdemo/controller/ScopeController.java
+++ src/main/java/com/springboot/springbootdemo/controller/ScopeController.java
@@ -1,4 +1,25 @@
 package com.springboot.springbootdemo.controller;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Scope("prototype")
 public class ScopeController {
+
+    public int number = 0;
+
+    @RequestMapping("/text1")
+    public String test1(){
+        number=number+1;
+        return String.valueOf(number);
+    }
+
+    @RequestMapping("/text2")
+    public String test2(){
+        number=number+1;
+
+        return String.valueOf(number);
+    }
 }
